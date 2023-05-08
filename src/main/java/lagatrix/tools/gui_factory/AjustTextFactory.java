187f00566cc7b73aaa7ada80fd95014225688956
Fco Javier Fraga Hernández <pachi030919@gmail.com>
2023-05-08 17:41:45 +0200

    Add the CorrectDialog window

---
+++ src/main/java/lagatrix/tools/gui_factory/AjustTextFactory.java
@@ -0,0 +1,26 @@
+package lagatrix.tools.gui_factory;
+
+/**
+ * AJust text in dialog.
+ *
+ * @author javierfh03
+ * @since 0.3
+ */
+public class AjustTextFactory {
+    
+    /**
+     * AJust text to see in dialog.
+     * 
+     * @param text The text who ajust.
+     * @return The text formated.
+     */
+    public static String ajust(String text) {
+        if (text.length() > 27) {
+            return String.format("<html>%s<br>%s</html>", 
+                    text.substring(0, 26),
+                        text.substring(26, text.length() - 1));
+        }
+        
+        return text;
+    }
+}
